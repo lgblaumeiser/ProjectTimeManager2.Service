584ed41ac53bab1f8d0ab644e9003514f204c5d0
Lars <lars@lgblaumeiser.com>
2020-06-17 21:31:12 +0200

    Switch build.gradle file to Kotlin Script
    Signed-off-by: Lars <lars@lgblaumeiser.com>

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+// SPDX-FileCopyrightText: 2020 Lars Geyer-Blaumeiser <lars@lgblaumeiser.de>
+// SPDX-License-Identifier: MIT
+
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    java
+    id("org.jetbrains.kotlin.jvm") version "1.3.72"
+}
+
+group = "de.lgblaumeiser.ptm"
+version = "2.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:3.4.2") // for kotest framework
+    testImplementation("io.kotest:kotest-assertions-core-jvm:3.4.2") // for kotest core jvm assertions
+    testImplementation("io.kotest:kotest-property-jvm:3.4.2") // for kotest property test
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
